254cef0e3d9d52d0732c57d9d98be6fa1fdba257
Autum Perconti <autumlucille@gmail.com>
2020-10-06 20:17:31 -0700

    adding data import

---
+++ static/app.js
@@ -0,0 +1,5 @@
+// Use D3 fetch to read the JSON file
+// The data from the JSON file is arbitrarily named importedData as the argument
+d3.json("data/data.json").then((importedData) => {
+    // console.log(importedData);
+    var data = importedData;
